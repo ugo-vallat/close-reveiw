1ba88ce93bbbff1beb90dc1cc60065b5a3b245ee
Daoud LAMALMI <daoudlamalmi@gmail.com>
2024-05-25 11:36:17 +0200

    updated config

--- include/utils/config.h
+++ include/utils/config.h
@@ -9,29 +9,42 @@
 #define BUFFER_SIZE 512
 
 typedef struct {
+    bool is_defined;
     char ip[CHAR_IP_SIZE];
     int port;
 } t_addr_serv;
 
 typedef struct {
+    bool is_defined;
+
     char ip[CHAR_IP_SIZE];
     int local_port;
     int public_port;
 } t_addr_user;
 
 typedef struct {
+    bool is_defined;
+
     bool user_consent;
     char path[BUFFER_SIZE];
 } t_history;
 
 typedef struct {
+    bool is_defined;
+
     char certificate[BUFFER_SIZE];
     char key[BUFFER_SIZE];
 } t_conf_ssl;
 
+typedef struct {
+    t_addr_user user;
+    t_addr_serv server;
+    t_history history;
+    t_conf_ssl config_ssl;
+} t_config;
+
 bool getConfigFilePath(char config_file[DIRECTORY_MAX_SIZE]);
 
-bool setConfig(FILE *config, t_addr_user *user, t_addr_serv *server, t_history *history,
-               t_conf_ssl *conf_ssl);
+bool setConfig(FILE *config, t_config *configuration);
 
 #endif // !__CONFIG_H__
--- src/utils/config.c
+++ src/utils/config.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 #include <utils/config.h>
+#include <utils/logger.h>
+
+#define FILE_CONFIG "config.c"
 
 bool getConfigFilePath(char config_file[DIRECTORY_MAX_SIZE]) {
+    char FUN_NAME[32] = "getConfigFilePath";
     char current_dir[DIRECTORY_MAX_SIZE];
     memset(config_file, 0, DIRECTORY_MAX_SIZE);
     if (getcwd(current_dir, DIRECTORY_MAX_SIZE) == NULL) {
-        perror("Couldn't get current working directory");
+        warnl(FILE_CONFIG, FUN_NAME, "Couldn't get current working directory");
         return false;
     }
     char *needle = strstr(current_dir, "close-review");
     if (needle == NULL) {
-        fprintf(stderr, "Directory 'close-review' not found in the current path\n");
+        warnl(FILE_CONFIG, FUN_NAME, "Directory 'close-review' not found in the current path");
         return false;
     }
     needle += strlen("close-review");
@@ -24,43 +29,53 @@ bool getConfigFilePath(char config_file[DIRECTORY_MAX_SIZE]) {
     return true;
 }
 
-bool setConfig(FILE *config, t_addr_user *user, t_addr_serv *server, t_history *history,
-               t_conf_ssl *conf_ssl) {
+t_config *initConfig(FILE *config) {
+    char FUN_NAME[32] = "initConfig";
     char header[BUFFER_SIZE];
     char token[BUFFER_SIZE];
+    t_config *configuration = malloc(sizeof(t_config));
 
     while (fgets(header, sizeof(header), config)) {
         if (sscanf(header, "[ %[^]] ]", token) == 1) {
             if (strncmp(token, "user", BUFFER_SIZE) == 0) {
-                if (fscanf(config, "ip = \"%15[^\"]\"\n", user->ip) != 1) {
-                    // User Configuration isn't mandatory
-                    user->ip[0] = '\0';
-                    user->local_port = -1;
-                    user->public_port = -1;
-                } else if (fscanf(config, "local_port = %d\n", &user->local_port) != 1 ||
-                           fscanf(config, "public_port = %d\n", &user->public_port) != 1) {
-                    perror("Error reading user section");
-                    return false;
+                configuration->user.is_defined =
+                    fscanf(config, "ip = \"%15[^\"]\"\n", configuration->user.ip) == 1 &&
+                    fscanf(config, "local_port = %d\n", &configuration->user.local_port) == 1 &&
+                    fscanf(config, "public_port = %d\n", &configuration->user.public_port) == 1;
+                if (!configuration->user.is_defined) {
+                    warnl(FILE_CONFIG, FUN_NAME, "Error reading user section");
+                    configuration->user.ip[0] = '\0';
+                    configuration->user.local_port = -1;
+                    configuration->user.public_port = -1;
                 }
             } else if (strncmp(token, "server", BUFFER_SIZE) == 0) {
-                if (fscanf(config, "ip = \"%15[^\"]\"\nport = %d\n", server->ip, &server->port) !=
-                    2) {
-                    perror("Error reading user section");
-                    return false;
+                configuration->server.is_defined =
+                    fscanf(config, "ip = \"%15[^\"]\"\n", configuration->server.ip) == 1 &&
+                    fscanf(config, "port = %d\n", &configuration->server.port) == 1;
+                if (!configuration->server.is_defined) {
+                    warnl(FILE_CONFIG, FUN_NAME, "Error reading user section");
+                    configuration->server.ip[0] = '\0';
+                    configuration->server.port = -1;
                 }
             } else if (strncmp(token, "history", BUFFER_SIZE) == 0) {
                 char consent[BUFFER_SIZE];
-                if (fscanf(config, "user_consent = %s\npath = \"%[^\"]\"\n", consent,
-                           history->path) != 2) {
-                    perror("Error reading user section");
-                    return false;
+                configuration->history.is_defined =
+                    fscanf(config, "user_consent = %s\n", consent) == 1 &&
+                    fscanf(config, "path = \"%[^\"]\"\n", configuration->history.path) == 1;
+                if (!configuration->history.is_defined) {
+                    warnl(FILE_CONFIG, FUN_NAME, "Error reading user section");
+                    configuration->history.user_consent = false;
+                    configuration->history.path[0] = '\0';
                 }
-                history->user_consent = (strcmp(consent, "true") == 0);
+                configuration->history.user_consent = (strcmp(consent, "true") == 0);
             } else if (strncmp(token, "ssl", BUFFER_SIZE) == 0) {
-                if (fscanf(config, "certificate = \"%[^\"]\"\nkey = \"%[^\"]\"\n",
-                           conf_ssl->certificate, conf_ssl->key) != 2) {
-                    perror("Error reading user section");
-                    return false;
+                configuration->config_ssl.is_defined =
+                    fscanf(config, "certificate = \"%[^\"]\"\n", configuration->config_ssl.certificate) == 1 &&
+                    fscanf(config, "key = \"%[^\"]\"\n", configuration->config_ssl.key) == 1;
+                if (configuration->config_ssl.is_defined) {
+                    warnl(FILE_CONFIG, FUN_NAME, "Error reading user section");
+                    configuration->config_ssl.certificate[0] = '\0';
+                    configuration->config_ssl.key[0] = '\0';
                 }
             }
         }
@@ -68,9 +83,9 @@ bool setConfig(FILE *config, t_addr_user *user, t_addr_serv *server, t_history *
 
     if (ferror(config)) {
         fclose(config);
-        perror("Error reading config file");
-        return false;
+        warnl(FILE_CONFIG, FUN_NAME, "Error reading config file");
+        return NULL;
     }
 
-    return true;
+    return configuration;
 }
